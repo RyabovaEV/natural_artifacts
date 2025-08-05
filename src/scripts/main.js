f5becde05974018b377ecf25960a1ebc4f89bbd6
elena.ryabova77m@gmail.com <elena.ryabova77m@gmail.com>
2025-08-05 11:04:55 +0300

    the site is ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 import 'virtual:svg-icons-register'
-import Slider from './Slider'
 import '../style/style.scss'
+import Slider from './Slider'
+import '../style/swiper.css'
 import DateCopyright from "./DateCopyright"
 import CheckForm from "./CheckForm"
 
